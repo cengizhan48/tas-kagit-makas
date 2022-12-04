5a5bddc134b8297974445ed051d9ba2009d80207
Cengizhan Karaduman <cengizhankarad@gmail.com>
2022-12-04 21:25:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let tas=document.getElementById("tas")
+let kagit=document.getElementById("kagit")
+let makas=document.getElementById("makas")
+let pcSelections=document.getElementById("pcSelections")
+let conclusion=document.getElementById
