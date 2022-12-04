832e38c6a4c842acfac5ad19c39bab45b4def1f7
Cengizhan Karaduman <cengizhankarad@gmail.com>
2022-12-04 21:28:21 +0300

    secend commit

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
+
 let tas=document.getElementById("tas")
 let kagit=document.getElementById("kagit")
 let makas=document.getElementById("makas")
 let pcSelections=document.getElementById("pcSelections")
-let conclusion=document.getElementById
+let conclusion=document.getElementById("conclusion")
+
+
+function selections(){
+
+    pcSelections=(Math.round(Math.random()*2))
+
+    if
+    (pcSelections==0 && playerSelections==0){
+        conclusion= "AYNI SEÇİM (BERABERE)"
+    }
+
+    else if
+    (pcSelections==0 && playerSelections==1){
+        conclusion="OYUNCU KAZANDI"
+    }
+     
+    else if
+    (pcSelections==0 && playerSelections==2){
+        conclusion="BİLGİSAYAR KAZANDI"
+    }
+
+    else if
+    (pcSelections==1 && playerSelections==0){
+        conclusion="BİLGİSAYAR KAZANDI"
+    }
+
+    else if
+    (pcSelections==1 && playerSelections==1){
+        conclusion="AYNI SEÇİM(BERABERE)"
+    }
+
+    else if
+    (pcSelections==1 && playerSelections==2){
+        conclusion="OYUNCU KAZANDI"
+    }
+
+    else if
+    (pcSelections==2 && playerSelections==0){
+        conclusion=="OYUNCU KAZANDI"
+    }
+
+    else if
+    (pcSelections==2 && playerSelections==1){
+        conclusion="BİLGİSAYAR KAZANDI"
+    }
+
+    else if
+    (pcSelections==2 && playerSelections==2){
+        conclusion="AYNI SEÇİM(BERABERE)"
+    }
+
+
+}
+
+selections();
+
